a02a7284aff4b03096269eba3e225fcc7376a097
Ljiljana <dstoj1@yahoo.comgit config --global user.email dstoj1@yahoo.com>
2017-09-28 13:17:23 -0500

    Updated About and Contact info

---
+++ RainEqualsOut/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(RainEqualsOut.Startup))]
+namespace RainEqualsOut
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
