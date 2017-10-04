22f3b634fc02654085f2a58eda0e38ad0701bfbf
Ljiljana <dstoj1@yahoo.comgit config --global user.email dstoj1@yahoo.com>
2017-10-04 08:46:03 -0500

    Created a MessageModel

---
+++ RainEqualsOut/Migrations/201710041341144_Created Message.Model.cs
@@ -0,0 +1,16 @@
+namespace RainEqualsOut.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class CreatedMessageModel : DbMigration
+    {
+        public override void Up()
+        {
+        }
+        
+        public override void Down()
+        {
+        }
+    }
+}
---
+++ RainEqualsOut/Models/Message.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+using System.Xml.Linq;
+
+namespace RainEqualsOut.Models
+{
+    public class Message
+    {
+            public int Id { get; set; }
+
+            [Display(Name = "Message")]
+            public string Msg { get; set; }
+
+            public ApplicationUser User { get; set; }
+        //    public Vendor Vendor { get; set; }
+        //}
+    }
+}
